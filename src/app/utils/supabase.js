47f3a5485d08ac699e0126cb81533a50e3449c41
Preeti Nag <preetinag017@gmail.com>
2025-09-26 13:11:40 +0530

    supa

--- src/app/utils/supabase.js
+++ src/app/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Replace with your actual Supabase URL and public key from your project settings
-const supabaseUrl = 'https://zzwvduqrkwjbyxpsjsqm.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6d3ZkdXFya3dqYnl4cHNqc3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4MTYzMTQsImV4cCI6MjA3NDM5MjMxNH0.xVvWp8fpX1aaEBtmeISDJzjkrNoL920sXrysmif3OyE'
+const supabaseUrl = "https://ywxfnndichtssscgtbsy.supabase.co"
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3eGZubmRpY2h0c3NzY2d0YnN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NjEyNjYsImV4cCI6MjA3NDQzNzI2Nn0.XvpebY8NpQTOJk77o1jOn7SYLGsrGM89bCo4C6OH4aE'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
